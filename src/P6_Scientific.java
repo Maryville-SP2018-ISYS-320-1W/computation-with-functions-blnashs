9db70d932ad83f7094ac21c57c84c8ec2fa7d1c0
Barbara Nash <bnash1@live.maryville.edu>
2018-04-01 19:03:02 -0500

    string 94

--- src/P6_Scientific.java
+++ src/P6_Scientific.java
@@ -8,7 +8,9 @@ public class P6_Scientific {
 
 	public static void main(String[] args) {
 		// REMINDER: Any print statements should only be in the main method
-
+		public double scientific(double base, double exp) {
+			return base * Math.pow(10, exp);
+			}
 	}
 
 }
